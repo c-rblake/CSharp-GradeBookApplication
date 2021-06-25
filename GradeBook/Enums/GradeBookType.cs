93299d1a15fc3fe3da4bd455cf540198f17f5d9b
Christoper <c.blake@hotmail.se>
2021-06-25 08:44:26 +0200

    step 1

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.Enums  // This should use the GradeBook.Enums namespace.
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,21 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name) // INVOKE BASE CONSTRUCTOR ON NAME
+            /*This constructor is required to invoke a constructor from BaseGradeBook.
+             * This can be done by adding : base(name) after the constructor declaration. 
+             * (this will make it so when the StandardGradeBook constructor is called 
+             * it will execute code in 
+             * both StandardGradeBook's constructor as 
+             * well as the BaseGradeBook's constructor)
+             * */
+        {
+            this.Type = GradeBookType.Standard;
+        }
+
+        
+    }
+}
